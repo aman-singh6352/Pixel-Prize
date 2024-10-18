0e867df7e43d07f2f883c49f22e7e8be39763205
aman-singh6352 <amansingh635217@gmail.com>
2024-10-18 11:56:29 +0530

    Updated

---
+++ main.js
@@ -0,0 +1,14 @@
+const icon = document.querySelector('.bar');
+const navlist = document.querySelector('.nav-list');
+let click = false;
+icon.addEventListener("click", function() {
+    if(click == false) {
+        navlist.style.display = "block";
+        click = true;
+    }
+    else {
+        navlist.style.display = "none";
+        click = false;
+    }
+})
+
